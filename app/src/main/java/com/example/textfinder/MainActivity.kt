3e6536be5fff8b95c9c9a880789dfb8dec6345d5
Bornik03 <bornikdekaviraj@gmail.com>
2024-07-21 02:22:03 +0530

    My ML Project

--- app/src/main/java/com/example/textfinder/MainActivity.kt
+++ app/src/main/java/com/example/textfinder/MainActivity.kt
@@ -1,140 +1,237 @@
 package com.example.textfinder
 
-import android.annotation.SuppressLint
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.textfinder.ui.theme.TextFinderTheme
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.File
 import java.io.InputStream
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TextFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    ImagePickerScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CameraPreviewScreen()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalCoilApi::class)
 @Composable
-fun ImagePickerScreen() {
+fun CameraPreviewScreen() {
     val context = LocalContext.current
-    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    var imageUri by remember { mutableStateOf<Uri?>(null) }
     var text by remember { mutableStateOf<String?>(null) }
-    var str by remember { mutableStateOf("Extract Text") }
-    val clipboardManager: androidx.compose.ui.platform.ClipboardManager = LocalClipboardManager.current
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? ->
-        imageUri = uri
+    var isLoading by remember { mutableStateOf(false) }
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasCameraPermission = isGranted
+    }
+
+    LaunchedEffect(Unit) {
+        if (!hasCameraPermission) {
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    if (hasCameraPermission) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CameraPreview { uri ->
+                isLoading=true
+                imageUri = uri
+                val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                inputStream?.close()
+                val inputImage = InputImage.fromBitmap(bitmap, 0)
+                recognizer.process(inputImage)
+                    .addOnSuccessListener { visionText ->
+                        isLoading=false
+                        text = visionText.text
+                        Log.d("CameraPreview", "Recognized text: $text")
+                        if(text=="")
+                        {
+                            Toast.makeText(context, "No text found", Toast.LENGTH_SHORT).show()
+                        }
+                        else
+                        {
+                            val intent = Intent(context, Predict::class.java).apply {
+                                putExtra("text1", text)
+                            }
+                            context.startActivity(intent)
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        isLoading=false
+                        text = "Error: ${e.message}"
+                        Log.d("CameraPreview", text!!)
+                    }
+            }
+            if (isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Camera permission is required")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { cameraPermissionLauncher.launch(Manifest.permission.CAMERA) }) {
+                Text(text = "Grant Permission")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CameraPreview(onImageCaptured: (Uri) -> Unit) {
+    val context = LocalContext.current
+    val lifecycleOwner = rememberUpdatedState(LocalContext.current as ComponentActivity)
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    val previewView = remember { PreviewView(context) }
+    val imageCapture = remember { ImageCapture.Builder().build() }
+
+    LaunchedEffect(cameraProviderFuture) {
+        try {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                lifecycleOwner.value,
+                cameraSelector,
+                preview,
+                imageCapture
+            )
+        } catch (e: Exception) {
+            Log.e("CameraPreview", "Failed to bind camera use cases", e)
+        }
     }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        imageUri?.let {
-            Column {
-                Image(
-                    painter = rememberImagePainter(data = it),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(200.dp)
-                        .clickable { launcher.launch("image/*") }
-                        .align(Alignment.CenterHorizontally).padding(top = 18.dp),
-                    contentScale = ContentScale.Crop
+        Spacer(modifier = Modifier.padding(top = 30.dp))
+        AndroidView(
+            modifier = Modifier
+                .size(height = 600.dp, width = 500.dp)
+                .padding(15.dp).clip(RoundedCornerShape(20.dp)),
+            factory = { previewView }
+        )
+        Spacer(modifier = Modifier.padding(top = 70.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {
+                val photoFile = File(
+                    context.externalMediaDirs.firstOrNull(),
+                    "${System.currentTimeMillis()}.jpg"
                 )
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = {
-                    if(str=="Extract Text")
-                    {
-                        val inputStream: InputStream? = context.contentResolver.openInputStream(it)
-                        val bitmap = BitmapFactory.decodeStream(inputStream)
-                        inputStream?.close()
-                        val inputImage = InputImage.fromBitmap(bitmap, 0)
+                val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
 
-                        recognizer.process(inputImage)
-                            .addOnSuccessListener { visionText ->
-                                text = visionText.text
-                            }
-                            .addOnFailureListener { e ->
-                                text = "Error: ${e.message}"
-                            }
-                        str="Re-Select Image"
-                    }
-                    else
-                    {
-                        launcher.launch("image/*")
-                        str="Extract Text"
-                    }
-                }, modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                    Text(text = str)
-                }
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                text?.let {
-                    Box(modifier = Modifier.padding(10.dp)) {
-                        LazyColumn {
-                            item {
-                                Text(it)
-                                Button(onClick = {
-                                    clipboardManager.setText(AnnotatedString(text!!))
-                                }) {
-                                    Text("Copy")
-                                }
-                            }
+                imageCapture.takePicture(
+                    outputOptions,
+                    ContextCompat.getMainExecutor(context),
+                    object : ImageCapture.OnImageSavedCallback {
+                        override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                            val savedUri = Uri.fromFile(photoFile)
+                            onImageCaptured(savedUri)
+                        }
+                        override fun onError(exception: ImageCaptureException) {
+                            Log.e("CameraCapture", "Image capture failed", exception)
                         }
                     }
-                }
+                )
+            }, modifier = Modifier.size(100.dp)) {
+                Text(text = "Capture")
             }
-        } ?: run {
-            Button(onClick = { launcher.launch("image/*") }) {
-                Text("Pick Image")
+            Spacer(modifier = Modifier.weight(.3f))
+            Button(onClick = {
+                val intent = Intent(context, Predict::class.java).apply {
+                    putExtra("text1", "")
+                }
+                context.startActivity(intent)
+            }) {
+                Text(text = "Pick \nImage")
             }
         }
     }
